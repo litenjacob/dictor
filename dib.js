ee77267a14d28d24b73464b735eed288fac9c6f2
Jacob Waller <litenjacob@gmail.com>
2009-09-24 21:52:07 +0200

    Getting touch enginge in place!

--- dib.js
+++ dib.js
@@ -17,7 +17,7 @@ function init(){
 	var head = document.getElementsByTagName('head')[0];
 	var dictorCSS = document.createElement('style');
 	dictorCSS.type = "text/css";
-	dictorCSS.textContent = '.dictor { cursor: pointer; } .dictorActive { background-color: #abc; } .dictorTranslation { font-size: 1.4em; background-color: #fff; -webkit-border-radius: 5px; padding: 4px; }';
+	dictorCSS.textContent = '.dictorLink.visible {display: block;} .dictorLink.hover { color: #f00;} .dictorLink {position:absolute; display:none;} .dictor {cursor:pointer;margin:0;padding:0;} .dictorActive {text-shadow: 0px 0px 2px #0a0;}';
 	head.appendChild(dictorCSS);
 	
 	Array.prototype.map = function(fn){
@@ -31,7 +31,8 @@ function init(){
 				
 	
 	var oldBody = body.innerHTML;
-	body.className = (body.className == "" ? "" : " ") + "dictorized";
+	//body.className = (body.className == "" ? "" : " ") + "dictorized";
+	addClass(body, "dictorized");
 	body.innerHTML = body.innerHTML.split(/[<>]/).map(function(n, i){
 		if (i % 2) {
 			return "<" + n + ">";
@@ -43,33 +44,119 @@ function init(){
 	
 	var middle = (new Date()).getTime() / 1000;
 	var spans = Array.prototype.slice.call(document.getElementsByClassName('dictor')).map(function(n){
-		n.onclick = translate;
+		n.onmousedown = touchdown;
 		return n;
 	});
 	
-	var from, to;
+	var link = document.createElement('div');
+	link.className = 'dictorLink';
+	body.appendChild(link);
+	
+	
+	
+	var from, to, translated = false, over = false;
+	document.onmousemove = function(e){
+		if(!link.isVisible){
+			return false;
+		}
+		if(isOverElem(e, link.location)){
+			if(!over){
+				addClass(link, 'hover');
+				over = true;
+			}
+			
+		} else {
+			if(over){
+				removeClass(link, 'hover');
+			}
+			over = false;
+		}
+	}
+	
+	document.onmouseup = touchup;
+	
+	function isOverElem(e, location){
+		return e.clientX > location.xs && e.clientX < location.xe && e.clientY > location.ys && e.clientY < location.ye;
+	}
+	
+	function touchdown(e){
+		if(translated){
+			translated.map(function(n){ removeClass(n, 'dictorActive'); return false; });
+			translated = false;
+		}
+		if(!from){
+			from = this;
+			from.location = findPos(from);
+		} else {
+			to = this;
+		}
+		
+		if (this.parentNode.nodeName == 'A') {
+			var p = findPos(this);
+			var a = this.parentNode;
+			link.textContent = "Goto: " + a.textContent;
+			link.url = a.getAttribute('href');
+			link.style.left = p.xs + 'px';
+			link.style.top = (p.ys - this.scrollHeight) + 'px';
+			addClass(link, 'visible');
+			link.isVisible = true;
+			link.location = findPos(link);
+
+			return false;
+		}
+		
+	}
+	
+	function touchup(e){		
+		if(link.isVisible){
+			if(isOverElem(e, link.location)){
+				console.log('goto ' + link.url);
+				return false;
+			} 
+		}
+
+		if(isOverElem(e, from.location)){
+			translate()
+		} else {
+			if(from && to){
+				translate();
+			} else {
+				console.log('flash from!');
+			}
+		}
+		
+		if (link.isVisible) {
+			link.isVisible = false;
+			removeClass(link, 'visible');
+		}
+	}
 	
 	function translate(){
-		if (this.parentNode.nodeName == 'A') {
-			console.log('pressed a link!', findPos(this));
-			link(this);
-		}
-		var tString = this.textContent.match(/[\w\'–\-]+/)[0];
-		console.log(tString, spans.indexOf(this));
+		console.log("translating");
+		to = to || from;
+		var fromClick = spans.indexOf(from);
+		var toClick = spans.indexOf(to);
+		//console.log(fromClick, toClick);
+		translated = spans.slice(Math.min(fromClick, toClick), Math.max(fromClick, toClick) + 1);
+		tString = translated.slice(0).map(function(n){ addClass(n, "dictorActive"); return n.textContent }).join(" ");
+		console.log("tString", tString);
+		from = to = false;
+		//var tString = this.textContent.match(/[\w\'–\-]+/)[0]; //really necessary? skip this...
+		//console.log(tString, spans.indexOf(this));
 		
-		if(!from){
+		/*if(!from){
 			from = this;
 		} else {
 			to = this;
-			tString = spans.slice(spans.indexOf(from), spans.indexOf(to) + 1).map(function(n){ return n.textContent }).join(" ");
+			
 			console.log(tString);
 		}
-		return false;
+		
+		addClass(this, "dictorActive");
+		
+		return false;*/
 	}
 	
-	function link(){
-		console.log("linking!");
-	}
 	
 	var after = (new Date()).getTime() / 1000;
 	console.log("Middle took " + (middle - before).toFixed(4) + " seconds");
@@ -77,7 +164,16 @@ function init(){
 	
 }
  
+function addClass(elem, newClass){
+	elem.className = (elem.className == "" ? "" : elem.className + " ") + newClass;
+}
+
+function removeClass(elem, oldClass){
+	elem.className = elem.className.replace(new RegExp("(\\s|^)" + oldClass + "(\\s|$)"), '');
+}  
+ 
 function findPos(obj){
+	var origObj = obj;
     var curleft = curtop = 0;
     if (obj.offsetParent) {
         do {
@@ -88,8 +184,10 @@ function findPos(obj){
     }
     
     return {
-        x: curleft,
-        y: curtop
+        xs: curleft,
+        ys: curtop,
+		xe: curleft + origObj.scrollWidth,
+		ye: curtop + origObj.scrollHeight
     };
     
 }
