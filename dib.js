c0ab04a920c14fc48046eecef9924316a330dda6
Jacob Waller <litenjacob@gmail.com>
2009-09-24 22:19:35 +0200

    Commenting and cleaning up

--- dib.js
+++ dib.js
@@ -12,14 +12,16 @@ init();
 function init(){
 	var before = (new Date()).getTime() / 1000;
 	var body = document.getElementsByTagName('body')[0];
-	if (body.className.match(/dictorized/)) { return false; }
+	if (body.className.match(/dictorized/)) { return false; } // already dictorized?
 	
+	// append css
 	var head = document.getElementsByTagName('head')[0];
 	var dictorCSS = document.createElement('style');
 	dictorCSS.type = "text/css";
 	dictorCSS.textContent = '.dictorLink.visible {display: block;} .dictorLink.hover { color: #f00;} .dictorLink {position:absolute; display:none;} .dictor {cursor:pointer;margin:0;padding:0;} .dictorActive {text-shadow: 0px 0px 2px #0a0;}';
 	head.appendChild(dictorCSS);
 	
+	// helper method
 	Array.prototype.map = function(fn){
 	    var arr = this, i = 0;
 	    this.forEach(function(n){
@@ -29,9 +31,8 @@ function init(){
 	    return arr;
 	}
 				
-	
+	// magically wraps all words in dictor spans
 	var oldBody = body.innerHTML;
-	//body.className = (body.className == "" ? "" : " ") + "dictorized";
 	addClass(body, "dictorized");
 	body.innerHTML = body.innerHTML.split(/[<>]/).map(function(n, i){
 		if (i % 2) {
@@ -43,54 +44,54 @@ function init(){
 	}).join("");
 	
 	var middle = (new Date()).getTime() / 1000;
+	
+	// get all spans into variable and bind touchevents
 	var spans = Array.prototype.slice.call(document.getElementsByClassName('dictor')).map(function(n){
 		n.onmousedown = touchdown;
 		return n;
 	});
 	
+	// create link container
 	var link = document.createElement('div');
 	link.className = 'dictorLink';
 	body.appendChild(link);
 	
-	
-	
+	// init semi-global variables
 	var from, to, translated = false, over = false;
+	
+	// need to track *mouse* movements
 	document.onmousemove = function(e){
-		if(!link.isVisible){
-			return false;
-		}
+		if(!link.isVisible){ return false; }
 		if(isOverElem(e, link.location)){
 			if(!over){
 				addClass(link, 'hover');
 				over = true;
-			}
-			
+			}	
 		} else {
-			if(over){
-				removeClass(link, 'hover');
-			}
+			if(over){ removeClass(link, 'hover'); }
 			over = false;
 		}
 	}
 	
+	// bind touchup. duh
 	document.onmouseup = touchup;
 	
+	// are we over a certain prepared element?
 	function isOverElem(e, location){
 		return e.clientX > location.xs && e.clientX < location.xe && e.clientY > location.ys && e.clientY < location.ye;
 	}
 	
 	function touchdown(e){
-		if(translated){
+		if(translated){ // old translation? if so - clear its style
 			translated.map(function(n){ removeClass(n, 'dictorActive'); return false; });
 			translated = false;
 		}
-		if(!from){
+		if(!from){ // have we already touched down once?
 			from = this;
 			from.location = findPos(from);
-		} else {
-			to = this;
-		}
+		} else { to = this; }
 		
+		// if we're touching a link, show linkhelper to make it 'clickable'
 		if (this.parentNode.nodeName == 'A') {
 			var p = findPos(this);
 			var a = this.parentNode;
@@ -101,7 +102,6 @@ function init(){
 			addClass(link, 'visible');
 			link.isVisible = true;
 			link.location = findPos(link);
-
 			return false;
 		}
 		
@@ -115,14 +115,10 @@ function init(){
 			} 
 		}
 
-		if(isOverElem(e, from.location)){
-			translate()
-		} else {
-			if(from && to){
-				translate();
-			} else {
-				console.log('flash from!');
-			}
+		if(isOverElem(e, from.location)){ translate() } 
+		else {
+			if(from && to){ translate(); } 
+			else { console.log('flash from!'); }
 		}
 		
 		if (link.isVisible) {
@@ -132,36 +128,27 @@ function init(){
 	}
 	
 	function translate(){
-		console.log("translating");
 		to = to || from;
+		// what indexes did we tap?
 		var fromClick = spans.indexOf(from);
 		var toClick = spans.indexOf(to);
-		//console.log(fromClick, toClick);
+
+		// slice em and dice em!
 		translated = spans.slice(Math.min(fromClick, toClick), Math.max(fromClick, toClick) + 1);
+		
+		// get the string for translation
 		tString = translated.slice(0).map(function(n){ addClass(n, "dictorActive"); return n.textContent }).join(" ");
+		
 		console.log("tString", tString);
+		
+		// reset variables
 		from = to = false;
-		//var tString = this.textContent.match(/[\w\'–\-]+/)[0]; //really necessary? skip this...
-		//console.log(tString, spans.indexOf(this));
-		
-		/*if(!from){
-			from = this;
-		} else {
-			to = this;
-			
-			console.log(tString);
-		}
-		
-		addClass(this, "dictorActive");
-		
-		return false;*/
 	}
 	
 	
 	var after = (new Date()).getTime() / 1000;
 	console.log("Middle took " + (middle - before).toFixed(4) + " seconds");
 	console.log("Dictorizing took " + (after - before).toFixed(4) + " seconds");
-	
 }
  
 function addClass(elem, newClass){
